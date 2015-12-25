75b21987cc77bde920df91c41c9bd0b63fe027f3
Aviad Blumenfeld <aviadblu@gmail.com>
2015-12-25 22:17:59 +0200

    update docker ip to dev env

--- server/config/env/dev.js
+++ server/config/env/dev.js
@@ -1,5 +1,5 @@
 module.exports = {
-    pgConnectionStr: 'postgres://udo:udo1234@0.0.0.0/udo_db',
+    pgConnectionStr: 'postgres://udo:udo1234@192.168.99.100/udo_db',
     FBcallbackUrl: 'http://localhost:9090/auth/facebook/callback',
     port: 9090,
     app: null
