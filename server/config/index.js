8a17082ed3d95cfe696989da201260502abb2696
Aviad Blumenfeld <aviadblu@gmail.com>
2016-01-31 17:52:22 +0200

    fix oauth proxy issue

--- server/config/index.js
+++ server/config/index.js
@@ -13,5 +13,16 @@ for(var i in envConfig) {
 }
 
 config.env = env;
+// if behind proxy:
+if(env === 'ubuntu') {
+    var globalTunnel = require('global-tunnel');
+
+    globalTunnel.initialize({
+        host: '16.42.57.12',
+        port: 8080,
+        sockets: 50 // optional pool size for each http and https
+    });
+}
+
 
 module.exports = config;
