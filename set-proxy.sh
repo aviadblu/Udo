edb29493dd3ec60bfe7d5ad7d53386d42c1428ae
Aviad Blumenfeld <aviadblu@gmail.com>
2016-03-06 16:37:31 +0200

    bugs fix

--- set-proxy.sh
+++ set-proxy.sh
@@ -5,8 +5,8 @@ export https_proxy=http://web-proxy.il.hpecorp.net:8080
 export HTTP_PROXY=http://web-proxy.il.hpecorp.net:8080
 export HTTPS_PROXY=http://web-proxy.il.hpecorp.net:8080
 
-npm config set proxy "http.proxy http://web-proxy.il.hpecorp.net:8080"
-npm config set https-proxy "https.proxy http://web-proxy.il.hpecorp.net:8080"
+npm config set proxy "http://web-proxy.il.hpecorp.net:8080"
+npm config set https-proxy "http://web-proxy.il.hpecorp.net:8080"
 
 git config --global http.proxy http://web-proxy.il.hpecorp.net:8080
 git config --global https.proxy http://web-proxy.il.hpecorp.net:8080
